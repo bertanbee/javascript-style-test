de542eccdb1c230b3e26b33df6e590f2ca14bc70
bertanbee <52827517+bertanbee@users.noreply.github.com>
2020-01-24 16:31:48 -0300

    Add files via upload

---
+++ bundle.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var usuario = {
+  nome: 'Diego',
+  idade: 23,
+  endereco: {
+    cidade: 'Rio do Sul',
+    uf: 'SC',
+    pais: 'Brasil'
+  }
+};
---
+++ main.js
@@ -0,0 +1,10 @@
+const usuario = {
+    nome: 'Diego',
+    idade: 23,
+    endereco: {
+    cidade: 'Rio do Sul',
+    uf: 'SC',
+    pais: 'Brasil',
+    }
+   };
+const { ...nome } = new usuario
